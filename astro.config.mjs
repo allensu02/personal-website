192f56ad23e462398609fa0f8d8381d268382398
Allen Su <allenysu@gmail.com>
2026-01-23 21:14:45 -0800

    Add GitHub Pages deploy workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://allensu02.github.io',
+  base: '/personal-website',
+});
